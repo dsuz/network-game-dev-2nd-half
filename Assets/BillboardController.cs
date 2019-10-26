f5ed764d09cb8a6246f20d8017ad0a64f71a76ca
Daisuke Suzuki <bboydaisuke@gmail.com>
2019-10-27 01:56:21 +0900

    ４週目（課題）完了状態とパッケージを追加した

---
+++ Assets/BillboardController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BillboardController : MonoBehaviour
+{
+    void Start()
+    {
+        Vector3 scale = transform.localScale;
+        scale.x = scale.x * (-1);
+        transform.localScale = scale;
+    }
+
+    void LateUpdate()
+    {
+        transform.LookAt(Camera.main.transform);
+    }
+}
--- Assets/TankController.cs
+++ Assets/TankController.cs
@@ -18,16 +18,39 @@ public class TankController : MonoBehaviour
     GameObject m_cannonObject;
     /// <summary>RPC を呼び出すための PhotonView コンポーネント</summary>
     PhotonView m_photonView;
+    /// <summary>ライフの最大値（初期値）</summary>
+    [SerializeField] int m_maxLife = 30;
+    /// <summary>現在のライフ</summary>
+    int m_life;
+    /// <summary>ライフ表示</summary>
+    [SerializeField] UnityEngine.UI.Text m_lifeText;
+    /// <summary>同期間隔（単位：秒）</summary>
+    [SerializeField] float m_syncPeriod = 3f;
+    float m_timer;
 
     void Start()
     {
         m_rb = GetComponent<Rigidbody>();
         // RPC を呼び出すために PhotonView を取得しておく
         m_photonView = GetComponent<PhotonView>();
+        m_life = m_maxLife;
     }
 
     void Update()
     {
+        // 同期処理
+        m_timer += Time.deltaTime;
+        if (m_timer > m_syncPeriod)
+        {
+            if (m_photonView && m_photonView.IsMine)
+            {
+                int id = PhotonNetwork.LocalPlayer.ActorNumber;
+                object[] parameters = new object[] { id, m_life };
+                m_photonView.RPC("SyncLife", RpcTarget.All, parameters);
+                m_timer = 0;
+            }
+        }
+
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
 
@@ -74,9 +97,10 @@ public class TankController : MonoBehaviour
             {
                 // id を取得して
                 int id = PhotonNetwork.LocalPlayer.ActorNumber;
-                object[] parameters = new object[] { id, 10 };
+                //object[] parameters = new object[] { id, 10 };
                 // RPC で呼び出す
-                m_photonView.RPC("Damage", RpcTarget.All, parameters);  // RpcTarget.All は、ローカルもリモートも呼び出すというオプションである。
+                //m_photonView.RPC("Damage", RpcTarget.All, parameters);  // RpcTarget.All は、ローカルもリモートも呼び出すというオプションである。
+                Damage(id, 10);
             }
         }
     }
@@ -86,12 +110,41 @@ public class TankController : MonoBehaviour
     /// </summary>
     /// <param name="playerId">プレイヤー ID</param>
     /// <param name="damage">受けたダメージ</param>
-    [PunRPC]
+    //[PunRPC]
     void Damage(int playerId, int damage)
     {
         Debug.Log("player " + playerId.ToString() + " took " + damage.ToString() + " damage: ");
 
         // TODO: tank のライフを設定し、受けたダメージ分ライフを減らす
         // ライフが 0 になったらやられたとみなし、タンクのオブジェクトを破棄する
+        m_life -= damage;
+        
+        if (m_life < 1)
+        {
+            PhotonNetwork.Destroy(this.gameObject);
+        }
+        else
+        {
+            object[] parameters = new object[] { playerId, m_life };
+            // RPC で呼び出す
+            m_photonView.RPC("SyncLife", RpcTarget.All, parameters);
+        }
+    }
+
+    /// <summary>
+    /// ライフを同期する
+    /// ライフ表示を更新する
+    /// </summary>
+    /// <param name="playerId"></param>
+    /// <param name="currentLife"></param>
+    [PunRPC]
+    void SyncLife(int playerId, int currentLife)
+    {
+        m_life = currentLife;
+        Debug.Log("player " + playerId.ToString() + "'s life: " + m_life.ToString());
+        if (m_lifeText)
+        {
+            m_lifeText.text = m_life.ToString();
+        }
     }
 }
