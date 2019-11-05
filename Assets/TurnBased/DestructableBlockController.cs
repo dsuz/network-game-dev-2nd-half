eae5e4bfc9f69a5465f29e28ace0fc4b32ab5017
Daisuke Suzuki <bboydaisuke@gmail.com>
2019-11-05 16:28:53 +0900

    ターンベースゲームのサンプルを追加した

---
+++ Assets/TurnBased/DestructableBlockController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ブロックに追加するコンポーネント
+/// プラットホームから落ちたブロックを破棄する
+/// </summary>
+public class DestructableBlockController : MonoBehaviour
+{
+    void Update()
+    {
+        // プラットホームから落ちたら破棄する
+        if (this.transform.position.y < -1)
+        {
+            // ネットワークオブジェクトとして破棄せず普通に破棄してしまう
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/TurnBased/ExplosionController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+/// <summary>
+/// 爆発・ブロック破壊機能を追加するコンポーネント
+/// </summary>
+public class ExplosionController : MonoBehaviour
+{
+    /// <summary>ネットワークオブジェクトを破棄するための PhotonView の参照</summary>
+    PhotonView view;
+
+    void Start()
+    {
+        view = GetComponent<PhotonView>();
+
+        // ParticleSystem の再生が終わったら破棄する
+        ParticleSystem ps = GetComponent<ParticleSystem>();
+        Destroy(this.gameObject, ps.main.duration);
+    }
+
+    /// <summary>
+    /// ブロックに Trigger が当たったら破棄する
+    /// </summary>
+    /// <param name="other"></param>
+    private void OnTriggerEnter(Collider other)
+    {
+        // 自分のクライアントから、相手のオブジェクトをネットワークオブジェクトとして破棄する
+        if (view && view.IsMine)
+        {
+            if (other.gameObject.tag == "DestructableBlock")
+            {
+                // ブロックの PhotonView を取得し、所有権を取ってネットワークオブジェクトとして破棄する
+                PhotonView view = other.gameObject.GetComponent<PhotonView>();
+                view.TransferOwnership(PhotonNetwork.LocalPlayer.ActorNumber);
+                PhotonNetwork.Destroy(view);
+            }
+        }
+    }
+}
